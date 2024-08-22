af980c0512aa023cc97e3be0e525af7794ed7878
Pipeioo <115722528+Pipeioo@users.noreply.github.com>
2024-08-22 15:50:04 -0300

    sophy crack

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from typing import Optional
+from urllib.request import Request
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+from tensorflow.python.keras.models import Sequential, load_model
+from tensorflow.python.keras.layers import LSTM, Dense
+from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+model = load_model('modelo4.h5')
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+@app.post("/rutamodelo/modelo")
+async def predict(request: Request):
+    data = await request.varchar()
+    
+    
     return {"item_id": item_id, "q": q}
+
+
+image_bytes = await file.read()
+image = image.open(io.BytesIO(image_bytes))
+image_np = np.array(image)
