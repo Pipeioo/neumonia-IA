fedb3b50f534486584fe1c8d90a6613925f6b185
Pipeioo <115722528+Pipeioo@users.noreply.github.com>
2024-08-23 08:41:40 -0300

    dads

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-"""
+
+from cmath import log
 from typing import Optional
 from urllib.request import Request
 from fastapi import FastAPI, File, UploadFile
@@ -36,8 +37,11 @@ async def xdd():
     image = Image.open(io.BytesIO(image_bytes))
     image_np = np.array(image)
     return image_bytes
-    """
 
+
+"""
+from cmath import log
+from distutils.log import Log
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from PIL import Image
 from tensorflow.python.keras.models import Sequential, load_model
@@ -56,8 +60,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Carga el modelo de IA
-model = load_model('modelo4.h5')
+modelo = load_model('models/modelo4.h5')
 
 @app.get("/")
 async def root():
@@ -66,17 +69,16 @@ async def root():
 @app.post("/rutamodelo/modelo")
 async def predict(file: UploadFile = File(...)):
     try:
-        # Lee la imagen subida
         image_bytes = await file.read()
         image = Image.open(io.BytesIO(image_bytes))
         
-        # Convertir imagen a numpy array
         image_np = np.array(image)
 
-        # Realiza la predicción directamente con el modelo
-        predictions = model.predict(image_np)
+        predictions = modelo.predict(image_np)
         predicted_class = np.argmax(predictions, axis=1)[0]
 
         return {"predicted_class": int(predicted_class)}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+"""
