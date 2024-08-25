8479deb2f2661439ecfd08be0f2b24e7d4e9429b
Pipeioo <115722528+Pipeioo@users.noreply.github.com>
2024-08-24 21:12:30 -0300

    media conexion

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
-
+"""
 from cmath import log
 from typing import Optional
 from urllib.request import Request
-from fastapi import FastAPI, File, UploadFile
-from PIL import Image, ImageFile
-from tensorflow.python.keras.models import Sequential, load_model
+
+
+
 from tensorflow.python.keras.layers import LSTM, Dense
 from fastapi.middleware.cors import CORSMiddleware
-
+"""
+from fastapi import FastAPI
+from PIL import Image, ImageFile
 app = FastAPI()
 
+"""
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3001"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-model = load_model('modelo4.h5')
+"""
+import keras
+"""from keras._tf_keras.keras.preprocessing.image import load_img"""
+from tensorflow.python.keras.models import Sequential, load_model
+from keras.src.utils.image_utils import load_img
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+def procesoImagen(urlImagen):
+
+    model = load_model('model/modelo.h5')
+    imagen = load_img(urlImagen, target_size=(150, 150))
+    imagen_array2 = img_to_array(imagen)
+    imagen_array2 = np.expand_dims(imagen_array2, axis=0)  # Añadir una dimensión para el batch
+    imagen_array2 /= 255.0  # Escalar los valores de los píxeles
+
+    # Hacer la predicción
+    prediccion = model.predict(imagen_array2)
+
+
+    return prediccion * 100
+
+@app.get("/modeloIA")
+async def get_image(imagen):
+    prediccion = procesoImagen(imagen)
+    return {"prediccion" : prediccion}
+
+
+"""
 @app.post("/rutamodelo/modelo")
 async def predict(request: Request):
     data = await request.varchar()
@@ -42,8 +68,8 @@ async def xdd():
     image_np = np.array(image)
     return image_bytes
 
+---
 
-"""
 from cmath import log
 from distutils.log import Log
 from fastapi import FastAPI, File, UploadFile, HTTPException
